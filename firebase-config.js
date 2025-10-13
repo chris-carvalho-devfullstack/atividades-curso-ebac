fc21d8ba93278499e8b86ab3f83a7d1506a35ee4
christian <christian.carvalho.adv@gmail.com>
2025-10-13 14:57:15 -0300

    Storagebucket iniciado

--- firebase-config.js
+++ firebase-config.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBsfGTZ1yypzWE4R_thDARSh61Osc6OUbU",
   authDomain: "gerenciador-tarefas-fd5be.firebaseapp.com",
   projectId: "gerenciador-tarefas-fd5be",
-  storageBucket: "gerenciador-tarefas-fd5be.appspot.com", // Verifique se o seu é .appspot.com
+  storageBucket: "gerenciador-tarefas-fd5be.appspot.com",
   messagingSenderId: "831715035671",
   appId: "1:831715035671:web:2836e1701f80fc6f602a52"
 };
