e27a3eac6429579edf1d420b5315d869a25efc22
christian <christian.carvalho.adv@gmail.com>
2025-10-13 16:28:24 -0300

    Teste de solução para firebase storage perfil

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
-// NOVAS IMPORTAÇÕES
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-storage.js";
 
+// ESTA É A CONFIGURAÇÃO CORRETA E COMPLETA
 const firebaseConfig = {
   apiKey: "AIzaSyBsfGTZ1yypzWE4R_thDARSh61Osc6OUbU",
   authDomain: "gerenciador-tarefas-fd5be.firebaseapp.com",
   projectId: "gerenciador-tarefas-fd5be",
-  storageBucket: "gerenciador-tarefas-fd5be.appspot.com",
+  storageBucket: "gerenciador-tarefas-fd5be.firebasestorage.app",
   messagingSenderId: "831715035671",
   appId: "1:831715035671:web:2836e1701f80fc6f602a52"
 };
